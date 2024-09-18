374da0e7c9b23ec158c09ab2355de859b272e00b
Gabriel <gabrielpachecotec@gmail.com>
2024-09-18 13:27:09 +0100

    Generate 16x16 grid of divs
    Added createSquareDiv() which creates a div with a width and heigth
    based on the dimensions of the client page. Added createGrid() which
    creates a 16x16 grid of divs. Added event listener on the window
    that resizes each div if the window is being resized.
    
    The HTML file just contains a container that is being populated by the
    grid of divs and the CSS is applying a display flex, so the divs wrap.

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+const container = document.querySelector(".container");
+const GENERAL_WIDTH = 16;
+const GENERAL_HEIGHT = 16;
+
+function createSquareDiv(width, height) {
+    const div = document.createElement('div');
+    div.style.width = `${width}px`;
+    div.style.height = `${height}px`;
+    div.style.border = "1px solid black";
+    return div;
+}
+
+function createGrid() {
+    const divWidth = container.clientWidth / GENERAL_WIDTH;
+    const divHeight = container.clientHeight / GENERAL_HEIGHT;
+    for (let i = 0; i < GENERAL_WIDTH; i++) {
+        for (let j = 0; j < GENERAL_HEIGHT; j ++) {
+            const div = createSquareDiv(divWidth, divHeight);
+            container.appendChild(div);
+        }
+    }
+}
+
+window.addEventListener("resize", () => {
+    const divWidth = container.clientWidth / GENERAL_WIDTH;
+    const divHeight = container.clientHeight / GENERAL_HEIGHT;
+    for (let i of container.childNodes) {
+        i.style.width = `${divWidth}px`;
+        i.style.height = `${divHeight}px`;
+    }
+})
+
+createGrid();
